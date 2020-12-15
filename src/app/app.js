c06026a450c2a26e57134d7e76a3d7f46562f607
Aaron Yu <ayybuddyy@gmail.com>
2020-12-15 16:55:14 -0500

    Add in temp navigation

--- src/app/app.js
+++ src/app/app.js
@@ -1,3 +1,42 @@
 'use strict';
+import { gsap } from 'gsap';
 
-console.log('Connected from app');
+const tl = gsap.timeline({ paused: true });
+
+tl.to('.one', {
+  duration: 0.5,
+  y: 6,
+  rotation: 45,
+  ease: 'expo.inOut',
+});
+tl.to('.two', {
+  duration: 0.5,
+  y: -6,
+  rotation: -45,
+  ease: 'expo.inOut',
+  delay: -0.5,
+});
+
+tl.to('.navbar', {
+  duration: 0.5,
+  top: '0%',
+  ease: 'power1.inOut',
+  // delay: 0.5,
+});
+tl.from('.navbar__link', {
+  duration: 0.8,
+  stagger: 0.3,
+  x: -200,
+  opacity: 0,
+  ease: 'power1.out',
+  clearProps: 'all',
+});
+tl.reverse();
+// gsap.set('.navbar__link', { clearProps: 'all' });
+
+const navLink = document.querySelector('.navbar__link');
+const toggleBtn = document.querySelector('.toggle-btn');
+toggleBtn.addEventListener('click', function () {
+  tl.reversed(!tl.reversed());
+  navLink.style.cssText = '';
+});
